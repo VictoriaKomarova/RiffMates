6fd7989ff1d70f7bef4bc184673d8e9bc8137c5f
VictoriaKomarova <99461265+VictoriaKomarova@users.noreply.github.com>
2025-05-19 22:28:42 +0700

    Add files via upload

---
+++ home/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def credits(request):
+    content = "Nicky\nVictoria"
+    return HttpResponse(content, content_type="text/plain")
+
+def news(request):
+    data ={"news": [
+        "RiffMates now has a new page!",
+        "RiffMates has its first page"
+    ]}
+    return render(request, "news.html", data)
